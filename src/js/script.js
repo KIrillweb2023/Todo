ec93e59f77592a24a7b20009b03f8e69f68a53dd
unknown <https://github.com/KIrillweb2023/Humanking.git>
2023-09-08 12:27:42 +0300

    todo commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,46 +1,32 @@
-class Task{
-    constructor(checkbox, descr, rename, trash, parent){
-        this.checkbox = checkbox;
-        this.descr = descr;
-        this.rename = rename;
-        this.trash = trash;
-        this.parent = document.querySelector(parent);
-    }
-    render(){
-        const element = document.createElement('div');
-        element.classList.add('main__tasking-task');
-        element.innerHTML = `
-            <div class="${this.checkbox}"></div>
-            <h5 class="main__tasking-task_descr">${this.descr}</h5>
-            <div class="main__tasking-task_rename">
-                <img src="${this.rename}" alt="">
-            </div>
-            <div class="main__tasking-task_delete">
-                <img src="${this.trash}" alt="">
-            </div>
-        `;
-        this.parent.append(element);
-       
-    }
+function taskHTML(checkbox, descr, rename, trash){
+    const element = document.createElement('div');
+    element.classList.add('main__tasking-task');
+    //помещаю верстку в элемент
+    element.innerHTML = `
+        <div class="${checkbox}"></div>
+        <h5 class="main__tasking-task_descr">${descr}</h5>
+        <div class="main__tasking-task_rename">
+            <img src="${rename}" alt="">
+        </div>
+        <div class="main__tasking-task_delete">
+            <img src="${trash}" alt="">
+        </div>
+    `;
+    //помещаем верстку в родителя элемента
+    const parent = document.querySelector('.main .main__tasking .container');
+    parent.append(element);
 }
-
-
-
-const button = document.querySelector('.main__add-btn');
 
 const arr = [];
 let id = 0;
-button.addEventListener('click', (e) =>{
+
+const btn = document.querySelector('.main__add-btn');
+btn.addEventListener('click', () =>{
     const input = document.querySelector('.main__add-input');
-    e.preventDefault();
-    if(input.value.trim() != ''){
-        arr.push({id: id, text: input.value});
-        new Task('main__tasking-task_checkbox', `${input.value}`, './icons/task/pencil (1) 1.svg', './icons/task/recycle-bin 1.svg', '.main .main__tasking .container').render();
-       
-        const checkbox = document.querySelector('.main__tasking-task_checkbox');
-        checkbox.addEventListener('click', (Event) => {
-            checkbox.classList.toggle('active');
-        });
+   
+    if(input.value.trim() != ""){
+        arr.push({ id: id, text: input.value });
+        taskHTML('main__tasking-task_checkbox checkbox', `${input.value}`, './icons/task/pencil (1) 1.svg', './icons/task/recycle-bin 1.svg');
 
         document.querySelectorAll('.main__tasking-task_delete').forEach((btn, i) =>{
             btn.addEventListener('click', () =>{
@@ -49,20 +35,14 @@ button.addEventListener('click', (e) =>{
                 console.log(arr);
             });
         });
-
+        document.querySelectorAll('.checkbox').forEach((btn, i) =>{
+            btn.addEventListener('click', () =>{
+                btn.classList.toggle('active');
+            });
+        });
+       
+    console.log(arr);
     }
-    input.value = '';
+    input.value = "";
     id++;
 });
-
-
-
-
-
-
-
-
-
-
-
-
